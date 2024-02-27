0da296f1b2fae6c3bd537d9f3fe820322bd9fa6f
Ammen1 <amenguda@gmail.com>
2024-02-27 22:16:24 +0300

    regist model in admin

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from.models import UserAccount,TeacherProfile,StudentProfile
+from django.contrib.auth.admin import UserAdmin
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'email', 'phone_number', 'is_active', 'role', 'is_superuser')
+    search_fields = ('email', 'phone_number')
+    
+    # Specify a valid field for ordering, for example, 'id'
+    ordering = ('id',)
+    actions = ['delete_selected']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name' ,'role', 'phone_number', 'display_pic')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Qualifications',{'fields': ('qualification','skills','subjects','category')})
+        # ('Important dates', {'fields': ('last_login',)}),
+    )
 
-# Register your models here.
+admin.site.register(UserAccount, CustomUserAdmin)
+admin.site.register(TeacherProfile)
+admin.site.register(StudentProfile)
