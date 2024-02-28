928cd17e4cc8ffcd722dc2e8804566ffb5179b87
Ammen1 <amenguda@gmail.com>
2024-02-28 20:30:43 +0300

    admin file update

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
 from django.contrib import admin
 from.models import UserAccount,TeacherProfile,StudentProfile
 from django.contrib.auth.admin import UserAdmin
