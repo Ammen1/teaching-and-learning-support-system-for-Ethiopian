bfd6a286e8ac886bfffea090f62a885d5fcef4f1
Ammen1 <amenguda@gmail.com>
2024-02-29 00:40:16 +0300

    update admin

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,36 +1,35 @@
 from django.contrib import admin
-from django.contrib import admin
-from.models import UserAccount,TeacherProfile,StudentProfile
-from django.contrib.auth.admin import UserAdmin
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import UserAccount
+from .models import User, Student, Parent
 
 
-class CustomUserAdmin(BaseUserAdmin):
-    list_display = ('email', 'phone_number', 'full_name', 'is_active', 'is_admin', 'is_staff', 'is_superuser')
-    search_fields = ('email', 'phone_number', 'full_name')
-    list_filter = ('is_active', 'is_admin', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "get_full_name",
+        "username",
+        "email",
+        "is_active",
+        "is_student",
+        "is_lecturer",
+        "is_parent",
+        "is_staff",
+    ]
+    search_fields = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "is_lecturer",
+        "is_parent",
+        "is_staff",
+    ]
 
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('full_name', 'phone_number', 'address', 'profile_pic', 'role')}),
-        ('Permissions', {'fields': ('is_active', 'is_admin', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-    )
+    class Meta:
+        managed = True
+        verbose_name = "User"
+        verbose_name_plural = "Users"
 
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'phone_number', 'full_name', 'password1', 'password2'),
-        }),
-    )
 
-    ordering = ('email',)
-    filter_horizontal = ('groups', 'user_permissions',)
-
-    # Exclude non-editable fields like 'date_joined' from form
-    readonly_fields = ('date_joined',)
-
-admin.site.register(UserAccount, CustomUserAdmin)
-admin.site.register(TeacherProfile)
-admin.site.register(StudentProfile)
+admin.site.register(User, UserAdmin)
+admin.site.register(Student)
+admin.site.register(Parent)
