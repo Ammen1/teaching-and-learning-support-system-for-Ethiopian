742c071b60673adc3e6672b0d3abbfd11c66ee50
Ammen1 <amenguda@gmail.com>
2024-02-29 16:25:06 +0300

    update urls

--- core/account/urls.py
+++ core/account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . import views
+from .views import StaffAddView, StudentAddView, EmailValidationOnForgotPasswordView, ParentAddView, ProfileUpdateView
+
+app_name = 'account' 
 
 urlpatterns = [
-    path('register/', views.UserRegister.as_view(), name='user-registration'),
-    path('generate-otp/', views.GenerateOTP.as_view(), name='generate_otp'),
-    path('verify-otp/', views.VerifyOTP.as_view(), name='verify_otp'),
-   
+    path('staff/add/', StaffAddView.as_view(), name='staff-add'),
+    path('student/add/', StudentAddView.as_view(), name='student-add'),
+    path('profile/update/<int:pk>/', ProfileUpdateView.as_view(), name='profile-update'),
+    path('forgot-password/', EmailValidationOnForgotPasswordView.as_view(), name='forgot-password'),
+    path('parent/add/', ParentAddView.as_view(), name='parent-add'),
 ]
