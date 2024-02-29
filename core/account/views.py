588798887d8f4a0795a275e6040df92cb6dcf0b4
Ammen1 <amenguda@gmail.com>
2024-02-29 16:26:35 +0300

    create staff , student, profileupdate, Emailvalidationon forgotpassword, add preant

--- core/account/views.py
+++ core/account/views.py
@@ -1,71 +1,121 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework import status,generics
-from .serializers import UserRegistrationSerializer,TeacherSerializer,StudentSerializer
-from.models import UserAccount, TeacherProfile,StudentProfile
-import random
+from .models import User, Parent
+from rest_framework import status
+from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+from django.conf import settings
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from rest_framework import serializers, status
+from course.models import Program
+from .serializers import StaffAddSerializer, StudentAddSerializer, ProfileUpdateSerializer, EmailValidationOnForgotPasswordSerializer, ParentAddSerializer
+from django.db import transaction
+
+
+class StaffAddView(generics.CreateAPIView):
+    serializer_class = StaffAddSerializer
+
+class StudentAddView(generics.CreateAPIView):
+    serializer_class = StudentAddSerializer
+
+    def perform_create(self, serializer):
+        program_id = self.request.data.get('program')  # Get the 'program' data from the request
+
+        # Convert the program_id to a Program instance
+        program_instance = Program.objects.get(pk=program_id)
+
+        # Perform the create operation, passing the 'program' instance to the serializer
+        serializer.save(program=program_instance)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Call perform_create to handle the 'program' field
+        self.perform_create(serializer)
+
+        return Response({"message": "User and Student created successfully."}, status=status.HTTP_201_CREATED)
+
+class ProfileUpdateView(generics.UpdateAPIView):
+    serializer_class = ProfileUpdateSerializer
+    queryset = User.objects.all()  
 
 
 
+class EmailValidationOnForgotPasswordView(generics.CreateAPIView):
+    serializer_class = EmailValidationOnForgotPasswordSerializer
 
-otp_storage = {}
-class GenerateOTP(APIView):
-    def post(self, request):
-        email = request.data.get('email')
-        print(email,1111111111111111111111111)
-        if email:
-            if UserAccount.objects.filter(email=email).exists():
-                return Response({'error': 'User with this email already exists'}, status=status.HTTP_400_BAD_REQUEST)
-            
-            otp = ''.join([str(random.randint(0, 9)) for _ in range(4)])
-            otp_storage[email] = otp 
-            print(otp,555555555555555555555555555555555)
-            Send the OTP to the user's email
-            send_mail('OTP Verification', f'Your OTP is: {otp}', 'amenguda@gmail.com', [email])
-            print(777777777777777)
-            return Response({'message': 'OTP sent successfully'}, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'Invalid email'}, status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-class VerifyOTP(APIView):
-    def post(self, request):
-        email = request.data.get('email')
-        entered_otp = request.data.get('otp')
+        try:
+            validated_email = serializer.validated_data['email']
+            user = User.objects.get(email__iexact=validated_email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("There is no user registered with the specified E-mail address.")
+        except User.MultipleObjectsReturned:
+            raise serializers.ValidationError("Multiple users found with the specified E-mail address. Contact support.")
 
-        if email in otp_storage:
-            stored_otp = otp_storage[email]
-            if entered_otp == stored_otp:
-                del otp_storage[email]  # remove the otps from the dictionary
-                return Response({'message': 'OTP verified successfully'}, status=status.HTTP_200_OK)
-            else:
-                return Response({'message': 'OTP verification failed'}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        # Generate a password reset token
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        token = default_token_generator.make_token(user)
+
+        # Save the token to the user model
+        user.password_reset_token = token
+        user.save()
+
+        # frontend URL here or retrieve it dynamicallyss
+        frontend_url = "http://127.0.0.1:8000/admin"  # Replace with your actual frontend URL
+
+        # Create a reset link with the token
+        reset_link = f"{frontend_url}/reset-password/{uid}/{token}/"
+
+        # Send an email with the reset link
+        send_mail(
+            "Password Reset",
+            f"Click the following link to reset your password: {reset_link}",
+            settings.EMAIL_FROM_ADDRESS,
+            [user.email],
+            fail_silently=False,
+        )
+
+        return Response({"message": "Reset link sent to the provided email address."}, status=status.HTTP_200_OK)
 
 
-class UserRegister(APIView):
-    permission_classes = []
-    parser_classes = (MultiPartParser, FormParser, JSONParser)
+class ParentAddView(generics.CreateAPIView):
+    serializer_class = ParentAddSerializer
 
-    def post(self, request):
-        print(request.data,2222222) 
-        serializer = UserRegistrationSerializer(data=request.data)
-        print(serializer,4444444)
-        if serializer.is_valid():
-            print(23232323232)
-            user = serializer.save()
-            print(user)
-            return Response({"message": "User registration successful."}, status=status.HTTP_201_CREATED)
-        print(serializer.errors)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def get(self, request):
-        users = UserAccount.objects.all()
-        serializer = UserRegistrationSerializer(users, many=True)
-        return Response(serializer.data)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Your logic to handle the serializer data and create a Parent instance
+        with transaction.atomic():
+            user_data = {
+                'first_name': serializer.validated_data['first_name'],
+                'last_name': serializer.validated_data['last_name'],
+                'address': serializer.validated_data['address'],
+                'phone': serializer.validated_data['phone'],
+                'email': serializer.validated_data['email'],
+                'is_parent': True
+            }
+
+            # Create User instance for the parent
+            user = User.objects.create(**user_data)
+
+            # Create Parent instance associated with User
+            parent = Parent.objects.create(
+                user=user,
+                student=serializer.validated_data['student'],
+                relation_ship=serializer.validated_data['relation_ship']
+            )
+
+        # Customize the response as needed
+        response_data = {
+            "message": "Parent created successfully.",
+            "parent_id": parent.id,
+        }
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
