e184764f12651eedd6ee6849fa692a80bcb5a1a0
Tamirat <amenguda@gmail.com>
2024-03-02 15:17:48 +0300

    urls for edit and delete

--- core/course/urls.py
+++ core/course/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
-from .views import ProgramAPIView, ProgramDetailView
+from .views import *
 
 urlpatterns = [
-    path('program/add/', ProgramAPIView.as_view()),
-    path('program/<int:pk>/', ProgramDetailView.as_view()),
+    path('program/add/', ProgramAPIView.as_view(), name='program_add'),
+    path('program/<int:pk>/', ProgramDetailView.as_view(), name='program_edit'),
+    path('program/<int:pk>/edit/', ProgramEditView.as_view(), name='program_edit_api'),
+    path('program/delete/<int:pk>/', ProgramDeleteView.as_view(), name='program_delete_api'),
 
 ]
