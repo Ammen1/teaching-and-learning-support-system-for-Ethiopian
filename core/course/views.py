4be1978eb7ac39b3a6b5032a6e25636865ec0480
Tamirat <amenguda@gmail.com>
2024-03-02 14:39:08 +0300

    create views for program and deatail views

--- core/course/views.py
+++ core/course/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Program
+from .serializers import ProgramSerializer
+from rest_framework.generics import RetrieveAPIView
+from rest_framework import permissions
+from .serializers import ProgramSerializer
 
-# Create your views here.
+
+
+
+class ProgramAPIView(CreateAPIView):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response({"detail": f"{serializer.data['title']} program has been created."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def program_add(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+
+
+class ProgramDetailView(RetrieveAPIView):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    lookup_field = 'pk'  # Adjust this based on your URL pattern
