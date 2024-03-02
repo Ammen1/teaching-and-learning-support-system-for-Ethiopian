da9d711bc81c5acc3f5efaee2bf2cf7a20fc0eb0
Tamirat <amenguda@gmail.com>
2024-03-02 15:16:49 +0300

    create ProgramEditview and delete

--- core/course/views.py
+++ core/course/views.py
@@ -1,11 +1,12 @@
-from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Program
 from .serializers import ProgramSerializer
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import permissions
 from .serializers import ProgramSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.generics import RetrieveUpdateAPIView, DestroyAPIView, CreateAPIView, RetrieveAPIView
 
 
 
@@ -31,4 +32,29 @@ class ProgramDetailView(RetrieveAPIView):
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
     permission_classes = [permissions.IsAuthenticated]
-    lookup_field = 'pk'  # Adjust this based on your URL pattern
+    lookup_field = 'pk' 
+
+
+class ProgramEditView(RetrieveUpdateAPIView):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    lookup_field = 'pk'
+    
+    
+
+
+from rest_framework.response import Response
+from rest_framework import status
+
+class ProgramDeleteView(DestroyAPIView):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
+    permission_classes = [IsAuthenticated]  # Adjust permissions as needed
+
+    def delete(self, request, *args, **kwargs):
+        program = self.get_object()
+        title = program.title
+        program.delete()
+        return Response({"detail": f"Program {title} has been deleted."}, status=status.HTTP_204_NO_CONTENT)
+   
