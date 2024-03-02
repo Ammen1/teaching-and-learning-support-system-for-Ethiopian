8745bf2b9faaf57b973ed18df9195c7e332b9b01
Ammen1 <amenguda@gmail.com>
2024-03-02 11:16:50 +0300

    update admin file

--- core/course/admin.py
+++ core/course/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
-from .models import Program
+from .models import Program, Course, CourseAllocation, Upload
 
 
 admin.site.register(Program)
+admin.site.register(Course)
+admin.site.register(CourseAllocation)
+admin.site.register(Upload)
