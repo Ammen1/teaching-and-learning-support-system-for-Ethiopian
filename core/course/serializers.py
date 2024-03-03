4309587c0e6dfa70e73951479d77dfb2fd7f93d8
Tamirat <amenguda@gmail.com>
2024-03-03 22:33:18 +0300

    update serializers

--- core/course/serializers.py
+++ core/course/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework import serializers  # Importing serializers from rest_framework
-from account.models import User  # Importing User model from accounts app
-from .models import Program, Course, CourseAllocation, Upload, UploadVideo  # Importing models from this app
+from rest_framework import serializers  
+from account.models import User  
+from .models import Program, Course, CourseAllocation, Upload, UploadVideo 
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,36 +19,27 @@ class UserSerializer(serializers.ModelSerializer):
    Serializer class for User model.
    """
    class Meta:
-       model = User  # Set the model as User
-       fields = "__all__"  # Include all fields
+       model = User  
+       fields = "__all__"  
+
 
 class CourseAllocationSerializer(serializers.ModelSerializer):
-   """
-   Serializer class for CourseAllocation model.
-   """
-   courses = serializers.PrimaryKeyRelatedField(
-       queryset=Course.objects.all().order_by("level"),  # Set queryset for courses
-       many=True,  # Allow multiple courses
-       read_only=False,  # Allow writing to the field
-   )
-   lecturer = serializers.PrimaryKeyRelatedField(
-       queryset=User.objects.filter(is_lecturer=True),  # Filter users to only include lecturers
-       write_only=True,  # Don't allow reading the field
-   )
+    courses = CourseSerializer(many=True)
+    lecturer = UserSerializer()
 
-   class Meta:
-       model = CourseAllocation  # Set the model as CourseAllocation
-       fields = ["lecturer", "courses"]  # Include only these fields
+    class Meta:
+        model = CourseAllocation
+        fields = ["lecturer", "courses"] 
 
 class UploadFormFileSerializer(serializers.ModelSerializer):
    """
    Serializer class for Upload model (file type).
    """
    class Meta:
-       model = Upload  # Set the model as Upload
+       model = Upload  
        fields = (
-           "title",  # Include title field
-           "file",  # Include file field
+           "title", 
+           "file", 
        )
 
 class UploadFormVideoSerializer(serializers.ModelSerializer):
@@ -56,11 +47,12 @@ class UploadFormVideoSerializer(serializers.ModelSerializer):
    Serializer class for Upload model (video type).
    """
    class Meta:
-       model = UploadVideo  # Set the model as UploadVideo
+       model = UploadVideo  
        fields = (
-           "title",  # Include title field
-           "video",  # Include video field
+           "title",  
+           "video",  
        )
        
        
+
 
