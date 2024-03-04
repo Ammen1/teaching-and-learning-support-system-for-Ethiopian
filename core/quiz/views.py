2d8cd9d99a624f9e20001bb74001749036000779
Tamirat <amenguda@gmail.com>
2024-03-04 23:56:11 +0300

    create views for create Qiuzcreate view

--- core/quiz/views.py
+++ core/quiz/views.py
@@ -1,12 +1,20 @@
-# views.py
-from rest_framework.views import APIView
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
-from rest_framework import status
+from .models import Course, Quiz
+from .serializers import QuizSerializer
+from django.shortcuts import get_object_or_404
 
+class QuizCreateAPIView(CreateAPIView):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = QuizSerializer
 
+    def get_queryset(self):
+        course = get_object_or_404(Course, slug=self.kwargs["slug"])
+        return Quiz.objects.filter(course=course)
 
-class ResultListAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_200_OK)
-
-  
+    def perform_create(self, serializer):
+        course = get_object_or_404(Course, slug=self.kwargs["slug"])
+        serializer.save(course=course)
