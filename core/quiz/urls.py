295c4edff7d50669dd69ff61b95124892c72a835
Tamirat <amenguda@gmail.com>
2024-03-05 02:15:48 +0300

    QuizDeleteapi mcqcreate add

--- core/quiz/urls.py
+++ core/quiz/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('<slug:slug>/quiz/create/', QuizCreateAPIView.as_view(), name='quiz-create'),
+    path('quizzes/<int:pk>/', QuizUpdateAPIView.as_view(), name='quiz-update-api'),
+    path('quizzes/<int:pk>/delete/', QuizDeleteAPIView.as_view(), name='quiz-delete-api'),
+    path('mc-questions/create/', MCQuestionCreateAPIView.as_view(), name='mc-question-create-api'),
+    path('choices/create/', ChoiceCreateAPIView.as_view(), name='choice-create-api'),
 ]
--- core/quiz/views.py
+++ core/quiz/views.py
@@ -2,9 +2,10 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
-from .models import Course, Quiz
-from .serializers import QuizSerializer
+from .models import Course, Quiz, MCQuestion, Choice
+from .serializers import *
 from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
 
 class QuizCreateAPIView(CreateAPIView):
     authentication_classes = [SessionAuthentication]
@@ -18,3 +19,44 @@ class QuizCreateAPIView(CreateAPIView):
     def perform_create(self, serializer):
         course = get_object_or_404(Course, slug=self.kwargs["slug"])
         serializer.save(course=course)
+
+
+class QuizUpdateAPIView(generics.UpdateAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+    permission_classes = [IsAuthenticated]  # Adjust as needed
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def get_object(self):
+        quiz_id = self.kwargs['pk']
+        return Quiz.objects.get(pk=quiz_id)
+
+class QuizDeleteAPIView(generics.DestroyAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+    permission_classes = [IsAuthenticated]  # Adjust as needed
+
+    def destroy(self, request, *args, **kwargs):
+        quiz = self.get_object()
+        quiz.delete()
+        return Response({'detail': 'Successfully deleted.'}, status=status.HTTP_204_NO_CONTENT)
+    
+    
+class MCQuestionCreateAPIView(generics.CreateAPIView):
+    queryset = MCQuestion.objects.all()
+    serializer_class = MCQuestionSerializer
+    permission_classes = [IsAuthenticated]  # Adjust as needed
+
+    def perform_create(self, serializer):
+        # Set the quiz field based on the URL parameter
+        quiz_id = self.kwargs.get('quiz_id')
+        serializer.save(quiz_id=quiz_id)
+
+class ChoiceCreateAPIView(generics.CreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+    permission_classes = [IsAuthenticated]  # Adjust as needed
+
+    
