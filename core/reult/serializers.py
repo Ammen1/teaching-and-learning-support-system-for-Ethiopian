34e28a5a66f4b909c7be3074953f7a733874bc35
Tamirat <amenguda@gmail.com>
2024-03-04 14:33:26 +0300

    create serializers

--- core/reult/serializers.py
+++ core/reult/serializers.py
@@ -1,44 +1,12 @@
 from rest_framework import serializers
-from .models import Question, Quiz, MCQuestion, Choice
+from .models import TakenCourse, Result
 
-class QuestionSerializer(serializers.Serializer):
-    answers = serializers.ChoiceField()
-
-class EssaySerializer(serializers.Serializer):
-    answers = serializers.CharField()
-
-class ChoiceSerializer(serializers.ModelSerializer):
+class TakenCourseSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Choice
-        fields = ['choice', 'correct']
-
-class MCQuestionSerializer(serializers.ModelSerializer):
-    choices = ChoiceSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = MCQuestion
+        model = TakenCourse
         fields = '__all__'
 
-class QuizAddSerializer(serializers.ModelSerializer):
-    questions = serializers.PrimaryKeyRelatedField(
-        queryset=Question.objects.all().select_subclasses(),
-        many=True,
-        required=False
-    )
-
+class ResultSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Quiz
-        fields = '__all__'
-
-    def create(self, validated_data):
-        questions_data = validated_data.pop('questions', [])
-        quiz = super().create(validated_data)
-        quiz.question_set.set(questions_data)
-        return quiz
-
-class QuizSerializer(serializers.ModelSerializer):
-    questions = QuestionSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Quiz
+        model = Result
         fields = '__all__'
