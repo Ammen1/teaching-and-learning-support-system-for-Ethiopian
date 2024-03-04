44802a9bee0add423993c167a8810e25f69f0288
Tamirat <amenguda@gmail.com>
2024-03-04 23:02:41 +0300

    add assessment_result and result_sheet_pdf_view urls

--- core/reult/urls.py
+++ core/reult/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('add-score/', AddScoreAPIView.as_view(), name='add_score_api'),
     path('add_score_for/<int:id>/', AddScoreForAPIView.as_view(), name='add_score_for_api'),
     path('grade_result/', GradeResultAPIView.as_view(), name='grade_result_api'),
+    path('assessment_result/', AssessmentResultAPIView.as_view(), name='assessment_result_api'),
+    path('result_sheet_pdf_view/<int:id>/', ResultSheetPDFAPIView.as_view(), name='result_sheet_pdf_view'),
 ]
