a0beed27e0030f211c3a1d97c83c634479505bbc
Tamirat <amenguda@gmail.com>
2024-03-04 14:34:38 +0300

    admin registiration for models

--- core/reult/admin.py
+++ core/reult/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import TakenCourse, Result
+
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = [
+        "student",
+        "course",
+        "assignment",
+        "mid_exam",
+        "quiz",
+        "attendance",
+        "final_exam",
+        "total",
+        "grade",
+        "comment",
+    ]
+
+
+admin.site.register(TakenCourse, ScoreAdmin)
+admin.site.register(Result)
