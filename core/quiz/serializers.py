b08172ee538f902c239801b32a11fa83b84b35cb
Tamirat <amenguda@gmail.com>
2024-03-04 23:55:19 +0300

    create serializers and update

--- core/quiz/serializers.py
+++ core/quiz/serializers.py
@@ -1,44 +1,51 @@
 from rest_framework import serializers
 from .models import Question, Quiz, MCQuestion, Choice
+from django.utils.translation import gettext_lazy as _
 
-class QuestionSerializer(serializers.Serializer):
-    answers = serializers.ChoiceField()
-
-class EssaySerializer(serializers.Serializer):
-    answers = serializers.CharField()
-
-class ChoiceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Choice
-        fields = ['choice', 'correct']
+class ChoiceSerializer(serializers.Serializer):
+    choice = serializers.CharField()
+    correct = serializers.BooleanField()
 
 class MCQuestionSerializer(serializers.ModelSerializer):
-    choices = ChoiceSerializer(many=True, read_only=True)
+    choices = ChoiceSerializer(many=True)
 
     class Meta:
         model = MCQuestion
-        fields = '__all__'
+        exclude = ()
 
-class QuizAddSerializer(serializers.ModelSerializer):
-    questions = serializers.PrimaryKeyRelatedField(
-        queryset=Question.objects.all().select_subclasses(),
-        many=True,
-        required=False
-    )
+class EssaySerializer(serializers.Serializer):
+    answers = serializers.CharField(style={'base_template': 'textarea.html'})
+
+class QuestionSerializer(serializers.Serializer):
+    answers = serializers.ListField(
+        child=serializers.CharField(), required=False
+    )  # Assuming answers is a list of strings
+
+class QuizSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True, required=False)
 
     class Meta:
         model = Quiz
-        fields = '__all__'
+        exclude = []
 
     def create(self, validated_data):
         questions_data = validated_data.pop('questions', [])
-        quiz = super().create(validated_data)
-        quiz.question_set.set(questions_data)
+        quiz = super(QuizSerializer, self).create(validated_data)
+
+        for question_data in questions_data:
+            answers_data = question_data.pop('answers', [])
+            question = Question.objects.create(quiz=quiz, **question_data)
+            question.answers.set(answers_data)
+
         return quiz
 
-class QuizSerializer(serializers.ModelSerializer):
-    questions = QuestionSerializer(many=True, read_only=True)
+    def update(self, instance, validated_data):
+        questions_data = validated_data.pop('questions', [])
+        instance = super(QuizSerializer, self).update(instance, validated_data)
 
-    class Meta:
-        model = Quiz
-        fields = '__all__'
+        for question_data in questions_data:
+            answers_data = question_data.pop('answers', [])
+            question = instance.question_set.create(**question_data)
+            question.answers.set(answers_data)
+
+        return instance
