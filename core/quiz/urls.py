69f19849627a1cad872bca71556cd2a040329c16
Tamirat <amenguda@gmail.com>
2024-03-04 23:56:39 +0300

    add urls for create quiz view

--- core/quiz/urls.py
+++ core/quiz/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('example/', ResultListAPIView.as_view(), name='example'),
-    # Add your URL patterns here
+    path('<slug:slug>/quiz/create/', QuizCreateAPIView.as_view(), name='quiz-create'),
 ]
