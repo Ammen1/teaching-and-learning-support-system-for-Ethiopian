d6f4fa421f426e6851c6861ff9ee867717372184
Tamirat <amenguda@gmail.com>
2024-03-05 21:15:01 +0300

    update app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// all components
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Company from "./Components/Company";
+import CourseOutlet from "./Components/CourseOutlet";
+import Status from "./Components/Status";
+import Instructor from "./Components/Instructor";
+import CourseCategory from "./Components/CourseCategory";
+import JoinCommunity from "./Components/JoinCommunity";
+import UpcomingWorkshop from "./Components/UpcomingWorkshop";
+import Review from "./Components/Review";
+import Support from "./Components/Support";
+import Footer from "./Components/Footer";
+import MobileNav from "./Components/MobileNav";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className=" overflow-hidden">
+      <MobileNav />
+      <Navbar />
+      <Hero />
+      <Company />
+      <CourseOutlet />
+      <Status />
+      <Instructor />
+      <CourseCategory />
+      <JoinCommunity />
+      <UpcomingWorkshop />
+      <Review />
+      <Support />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
