ac25cedeb51cc8be0f225bef5787a99fa9bdd427
Tamirat <amenguda@gmail.com>
2024-03-05 21:13:32 +0300

    add Instructor data

---
+++ frontend/src/Data/Instructor.js
@@ -0,0 +1,26 @@
+export const InstructorData = [
+  {
+    image: "/Instructor/instructor1.png",
+    name: "Christina Tailor",
+  },
+  {
+    image: "/Instructor/instructor2.png",
+    name: "Usman Yousaf",
+  },
+  {
+    image: "/Instructor/instructor3.png",
+    name: " Artem Maltsev",
+  },
+  {
+    image: "/Instructor/instructor4.png",
+    name: " Shipman Northcutt",
+  },
+  {
+    image: "/Instructor/instructor1.png",
+    name: " Usman Yousaf",
+  },
+  {
+    image: "/Instructor/instructor2.png",
+    name: "  Usman Yousaf",
+  },
+];
