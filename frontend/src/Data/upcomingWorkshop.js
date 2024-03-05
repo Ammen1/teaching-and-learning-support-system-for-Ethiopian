c702d8fe13dda9ee80465ea5d2e1ea01f516ca4e
Tamirat <amenguda@gmail.com>
2024-03-05 21:15:27 +0300

    upcoming data

---
+++ frontend/src/Data/upcomingWorkshop.js
@@ -0,0 +1,26 @@
+export const upcomingWorkshop = [
+  {
+    image: "/Workshop/blockchain.png",
+    title: "Blockchain Technology",
+    Speaker: " Md. Alvy",
+    date: "7 September 2022",
+  },
+  {
+    image: "/Workshop/robotics.png",
+    title: "AI in Robotics",
+    Speaker: "Eastiak Emon",
+    date: "7 September 2022",
+  },
+  {
+    image: "/Workshop/uxRoadmap.png",
+    title: "UX Roadmap",
+    Speaker: "Pritue Mondal",
+    date: "7 September 2022",
+  },
+  {
+    image: "/Workshop/robotics.png",
+    title: "AI in Robotics",
+    Speaker: "Eastiak Emon",
+    date: "7 September 2022",
+  },
+];
