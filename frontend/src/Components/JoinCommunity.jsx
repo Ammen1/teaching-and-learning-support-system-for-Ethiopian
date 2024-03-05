b6ef932bf62d5b3dd779ef6a67c85712414595a8
Tamirat <amenguda@gmail.com>
2024-03-05 23:45:20 +0300

    change the color and replace image with animation icon

--- frontend/src/Components/JoinCommunity.jsx
+++ frontend/src/Components/JoinCommunity.jsx
@@ -1,13 +1,19 @@
-import React from "react";
 
+import heroAnimation from '../animation/Animation 4.json';
+import Lottie from "lottie-react";
 const JoinCommunity = () => {
   return (
     <section className="w-full my-10 md:my-16 bg-[#F1F7FF] overflow-hidden">
       <div className="w-full max-w-[1600px] mx-auto px-6 py-8 md:py-24 flex items-center justify-between gap-10 flex-col md:flex-row">
-        <img src="community.png" alt="image" className="w-full md:w-[550px]" />
+      <Lottie
+        animationData={heroAnimation}
+        loop
+        autoplay
+        style={{ width: '100%', maxWidth: '600px' }}
+      />
 
         <div className="w-full md:w-[45%]">
-          <h2 className="text-[20px] md:text-[30px] font-[700] text-textColor">
+          <h2 className="text-[20px] md:text-[30px] font-[700] text-[#228be6]">
             Let’s Study Together
           </h2>
           <p className="text-[14px] md:text-[18px] font-[500] text-textColor mt-3 mb-8">
@@ -15,7 +21,7 @@ const JoinCommunity = () => {
             betterment. Join now & make your own study group to grow together &
             build community.
           </p>
-          <button className="py-3 px-6 bg-brandColor rounded-md text-[#fff]">
+          <button className="py-3 px-6 bg-[#228be6] rounded-md text-[#fff]">
             Join Community
           </button>
         </div>
