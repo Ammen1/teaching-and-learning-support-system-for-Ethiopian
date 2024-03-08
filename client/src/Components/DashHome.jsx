efca0318158131ab19fd369122f4c9b75f938a43
Tamirat <amenguda@gmail.com>
2024-03-08 23:54:40 +0300

    add the ThemProvider wrap all pages

--- client/src/Components/DashHome.jsx
+++ client/src/Components/DashHome.jsx
@@ -1,13 +1,21 @@
 import DashSidebar from './DashSidebar';
 import Header from './Header';
+import ThemeProvider from "../Components/ThemeProvider.jsx";
+import Courses from '../pages/Courses';
+import PostPage from '../pages/PostPage';
 
 export default function DashHome() {
   return (
     <> 
+    <ThemeProvider>
       <Header />
       <DashSidebar />
+    </ThemeProvider>
     </>
   );
 };
+
+
+
 
 
