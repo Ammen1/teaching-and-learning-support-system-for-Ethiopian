f1cc6a88dc1419ca508d66e34785c3e796c746cc
Tamirat <amenguda@gmail.com>
2024-03-08 23:51:57 +0300

    remove resposive part from Headers

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -49,21 +49,25 @@ export default function Header() {
   };
 
   return (
-    <Navbar className='border-b-2 p-2'>
-           <Link
-          to='/'
-          className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
-        >
-          <img src="Company/R.png" alt="logo" className="w-[150px] px-2 py-1 bg-gradient-to-r from-indigo-500 rounded-lg" />
-        </Link>
+    <Navbar className='border-b-2  p-4 dark:bg-[rgb(13,13,37)]'>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Dash
+        </span>
+        Board
+      </Link>
       <form onSubmit={handleSubmit}>
         <TextInput
           type='text'
           placeholder='Search...'
           rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
+          className='hidden lg:inline dark:bg-black'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          
         />
       </form>
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
@@ -71,7 +75,7 @@ export default function Header() {
       </Button>
       <div className='flex gap-2 md:order-2'>
         <Button
-          className='w-12 h-10 hidden sm:inline'
+          className='w-12 h-10 hidden sm:inline dark:bg-black'
           color='gray'
           pill
           onClick={() => dispatch(toggleTheme())}
@@ -105,19 +109,8 @@ export default function Header() {
             </Button>
           </Link>
         )}
-        <Navbar.Toggle />
+    
       </div>
-      <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'}>
-          <Link to='/'>Home</Link>
-        </Navbar.Link>
-        <Navbar.Link active={path === '/about'} as={'div'}>
-          <Link to='/about'>About</Link>
-        </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
-          <Link to='/projects'>Projects</Link>
-        </Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+        </Navbar>
   );
 }
