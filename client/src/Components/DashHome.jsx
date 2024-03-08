01c0018c2bacdf5f0332a94be991c9a406e98f8a
Tamirat <amenguda@gmail.com>
2024-03-08 18:15:28 +0300

    create DashHome for Dashboard ui

---
+++ client/src/Components/DashHome.jsx
@@ -0,0 +1,13 @@
+import DashSidebar from './DashSidebar';
+import Header from './Header';
+
+export default function DashHome() {
+  return (
+    <> 
+      <Header />
+      <DashSidebar />
+    </>
+  );
+};
+
+
