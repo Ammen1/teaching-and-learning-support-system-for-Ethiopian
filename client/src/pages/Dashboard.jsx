efd8e0e560706032e264ced666d88f857d6cd24e
Tamirat <amenguda@gmail.com>
2024-03-08 23:53:45 +0300

    update with new page add

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../Components/DashSidebar';
-
+import DashProfile from '../Components/DashProfile';
+import DashPosts from '../Components/DashPosts';
+import DashUsers from '../Components/DashUsers';
+import DashComments from '../Components/DashComments';
+import DashboardComp from '../Components/DashboardComp';
+import Courses from './Courses';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -29,6 +34,7 @@ export default function Dashboard() {
       {tab === 'comments' && <DashComments />}
       {/* dashboard comp */}
       {tab === 'dash' && <DashboardComp />}
+      {tab === 'course' && <Courses />}
     </div>
   );
 }
