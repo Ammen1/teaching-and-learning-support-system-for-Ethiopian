06b1c81a04d846a0e05a046d807d9920781ae929
Tamirat <amenguda@gmail.com>
2024-03-08 10:08:58 +0300

    update with new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./Components/Footer";
 import SignIn from './pages/SignIn';
 import Navbar from './Components/Navbar';
 import ScrollToTop from './Components/ScrollToTop';
+import Dashboard from './pages/Dashboard';
 
 
 
@@ -16,6 +17,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/dashboard' element= { <Dashboard />} />
       </Routes>
       <Footer />
     </BrowserRouter>
