625fe6ca9281d92b569de74d5e433c34e8eed7f8
Tamirat <amenguda@gmail.com>
2024-03-08 10:12:10 +0300

    urls for login view

--- core/account/urls.py
+++ core/account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import StaffAddView, StudentAddView, EmailValidationOnForgotPasswordView, ParentAddView, ProfileUpdateView
+from .views import UserLoginView, MyTokenObtainPairView
 
 app_name = 'account' 
 
 urlpatterns = [
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('staff/add/', StaffAddView.as_view(), name='staff-add'),
     path('student/add/', StudentAddView.as_view(), name='student-add'),
     path('profile/update/<int:pk>/', ProfileUpdateView.as_view(), name='profile-update'),
