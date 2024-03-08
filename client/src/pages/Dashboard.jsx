c5ea4ad65155cc23a8dc613e21644875385a85d0
Tamirat <amenguda@gmail.com>
2024-03-08 10:07:34 +0300

    create dashboard page

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+
+export default function Dashboard() {
+
+  return (
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        
+        DashSidebar 
+      </div>
+   
+    </div>
+  );
+}
