30cd9da0f0f43718f19f0e99ca019ff29777e3f0
Tamirat <amenguda@gmail.com>
2024-03-09 16:28:20 +0300

    move down  by 20

--- client/src/Components/DashPosts.jsx
+++ client/src/Components/DashPosts.jsx
@@ -73,7 +73,7 @@ export default function DashPosts() {
   };
 
   return (
-    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+    <div className="table-auto mt-20 overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className="shadow-md">
