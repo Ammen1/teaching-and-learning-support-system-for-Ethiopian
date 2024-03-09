3b73ea0e875fddf0646f89922b3e5353fd696baf
Tamirat <amenguda@gmail.com>
2024-03-09 16:27:10 +0300

    update DashHome with ThemeProvider

--- client/src/Components/DashHome.jsx
+++ client/src/Components/DashHome.jsx
@@ -3,13 +3,17 @@ import Header from './Header';
 import ThemeProvider from "../Components/ThemeProvider.jsx";
 import Courses from '../pages/Courses';
 import PostPage from '../pages/PostPage';
+import Resources from './Resources.jsx';
 
 export default function DashHome() {
   return (
     <> 
     <ThemeProvider>
-      <Header />
+      <div className=' w-screen lg:-translate-x-4'><Header /></div>
+      <div className='flex min-w-screen'>
       <DashSidebar />
+      </div>
+      
     </ThemeProvider>
     </>
   );
