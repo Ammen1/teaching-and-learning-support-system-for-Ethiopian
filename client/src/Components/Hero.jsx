4b6ae6fbb7e4e821c6ca9b0fcfa2b1132c153bbe
Tamirat <amenguda@gmail.com>
2024-03-28 10:28:27 +0300

    feat: Update Hero component with Lottie animation and button styles
    - Integrated Lottie animation into the Hero component
    - Updated button styles for better visual appearance and consistency

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,37 +1,32 @@
 import Lottie from "lottie-react";
 import heroAnimation from '../animation/Animation 2.json';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Button } from "flowbite-react";
 
 const Hero = () => {
   return (
     <header className="flex items-center justify-between gap-8 w-full max-w-[1600px] mx-auto px-6 py-6 md:py-16 flex-col md:flex-row">
       <div>
-        <h1 className=" text-textColor font-[700] text-[35px] md:text-[60px] leading-[3rem] md:leading-[4.5rem]">
+        <h1 className="text-textColor font-[700] text-[35px] md:text-[60px] leading-[3rem] md:leading-[4.5rem]">
           <span className="text-[#228be6]">Studying Online</span> Is Much
           Easier Now!
         </h1>
         <p className="text-[16px] md:text-[20px] font-[500] text-textColor mt-3 w-full md:w-[50%]">
-        Your E-Learning Platform Unlock the Power of Education in Ethiopia!
+          Your E-Learning Platform Unlock the Power of Education in Ethiopia!
         </p>
 
         <div className="flex items-center gap-5 mt-8">
-          <button className="">
-            
-          </button>
           <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' className="border-2 py-2 px-2 bg-[#228be6] rounded-md text-[#fff]">
-            Get Started
-          </Button>
-        </Link>
+            <Button gradientDuoTone='purpleToBlue' className="border-2 py-2 px-2 bg-[#228be6] rounded-md text-[#fff]">
+              Get Started
+            </Button>
+          </Link>
 
-        <Link to='/try-free'>
-          <Button gradientDuoTone='purpleToBlue' className="border-2 py-2 px-2 bg-[#228be6] rounded-md text-[#fff]">
-            Try Free
-          </Button>
-        </Link>
-
-
+          <Link to='/try-free'>
+            <Button gradientDuoTone='purpleToBlue' className="border-2 py-2 px-2 bg-[#228be6] rounded-md text-[#fff]">
+              Try Free
+            </Button>
+          </Link>
         </div>
       </div>
 
@@ -39,7 +34,8 @@ const Hero = () => {
         animationData={heroAnimation}
         loop
         autoplay
-        style={{ width: '80%', maxWidth: '600px' }} className=" rounded-lg overflow-hidden bg-cover"
+        style={{ width: '80%', height: "50%", maxWidth: '500px' }}
+        className="rounded-lg bg-cover"
       />
     </header>
   );
