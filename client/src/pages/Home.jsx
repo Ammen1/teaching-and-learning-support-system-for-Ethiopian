eb467c613ce17f0328a689ce4a175b09e54981df
Tamirat <amenguda@gmail.com>
2024-03-28 10:29:34 +0300

    refactor: Restructure component hierarchy in App.js
    - Moved Navbar component outside the main content div
    - Reorganized the order of components to improve component hierarchy

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,32 +2,25 @@
 // all components
 
 import Hero from "../Components/Hero";
-import Company from "../Components/Company";
 import CourseOutlet from "../Components/CourseOutlet";
 import Status from "../Components/Status";
-import Instructor from "../Components/Instructor";
 import CourseCategory from "../Components/CourseCategory";
 import JoinCommunity from "../Components/JoinCommunity";
-import UpcomingWorkshop from "../Components/UpcomingWorkshop";
 import Review from "../Components/Review";
 import Support from "../Components/Support";
 import MobileNav from "../Components/MobileNav";
-import Navbar from '../Components/Navbar';
-import { CiGlass } from "react-icons/ci";
+
 
 const App = () => {
   return (
     <div className=" dark:bg-white"> 
-      <Navbar />
       <MobileNav />
-      <Hero />
-      <Company />
+      <div className=""><Hero /></div>
+
       <CourseOutlet />
       <Status />
-      <Instructor />
       <CourseCategory />
       <JoinCommunity />
-      <UpcomingWorkshop />
       <Review />
       <Support />
     </div>
