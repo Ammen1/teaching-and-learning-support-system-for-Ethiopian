4d514a94e62590bbe2ccec8ca879c08a72b90fa8
Tamirat <amenguda@gmail.com>
2024-03-31 22:43:44 +0300

    add signup router and revome DshUser

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from "./Components/Footer";
 import SignIn from './pages/SignIn';
-import ScrollToTop from './Components/ScrollToTop';
+
 import Dashboard from './pages/Dashboard';
 import DashUsers from './Components/DashUsers';
 import PrivateRoute from "./Components/PrivateRoute"
 import Navbar from './Components/Navbar';
+import SignUp from './pages/SignUp';
+import SingleCoursePage from './Components/CoureseSingle';
 
 
 
@@ -14,11 +16,12 @@ import Navbar from './Components/Navbar';
 export default function App() {
   return (
     <BrowserRouter>
-      {/* <ScrollToTop /> */}
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/course/:slug' element={<SingleCoursePage />} />
         <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
