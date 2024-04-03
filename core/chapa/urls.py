eed4160a27db7e3ee09e882db120298d661f62fd
Tamirat <amenguda@gmail.com>
2024-04-03 08:59:54 +0300

    Added endpoint 'chapa-webhook/' for handling Chapa webhook events using ChapaWebhookView and int to str for verify-payment

--- core/chapa/urls.py
+++ core/chapa/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import InitiateChapaTransactionView
+from .views import InitiateChapaTransactionView, ChapaWebhookView
 from . import views
 
 urlpatterns = [
+    path('chapa-webhook/', ChapaWebhookView.as_view(), name='chapa_webhook'),
     path('initiate-chapa-transaction/', InitiateChapaTransactionView.as_view(), name='initiate_chapa_transaction'),
     path('send-request/', views.send_request, name='send_request'),
-    path('verify-payment/<int:transaction_id>/', views.verify_payment, name='verify_payment'),
+    path('verify-payment/<str:transaction_id>/', views.verify_payment, name='verify_payment'),
     # path('transaction/verify/<str:tx_ref>/', views.verify_chapa_transaction, name='verify_transaction'),
 ]
