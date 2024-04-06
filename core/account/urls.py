9cafec8cfee4dc274e86557cf730c311e488d9ed
Tamirat <amenguda@gmail.com>
2024-04-06 10:26:04 +0300

    Update urlpatterns in urls.py
    Added new paths and modified existing paths in urlpatterns to include additional functionality:
    
    - Added path for password reset confirmation using PasswordResetConfirmView
    - Added path for password reset completion using PasswordResetCompleteView
    - Modified path for admin login to use LoginView from django.contrib.auth.views
    - Updated the import statement for PasswordResetConfirmView
    - Removed the commented-out path for password reset confirmation using PasswordResetView
    
    These changes enhance the user authentication and password reset functionality of the application.

--- core/account/urls.py
+++ core/account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 from .views import UserLoginView, MyTokenObtainPairView
+from django.contrib.auth.views import PasswordResetConfirmView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
@@ -10,6 +13,10 @@ urlpatterns = [
     path('student/add/', StudentAddView.as_view(), name='student-add'),
     path('profile/update/<int:pk>/', ProfileUpdateView.as_view(), name='profile-update'),
     path('forgot-password/', EmailValidationOnForgotPasswordView.as_view(), name='forgot-password'),
+    # path('api/account/reset-password/<uidb64>/<token>/', PasswordResetView.as_view(), name='password_reset_confirm'),
+    path('reset-password/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset-password/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('admin/login/', auth_views.LoginView.as_view(), name='login'),
     path('parent/add/', ParentAddView.as_view(), name='parent-add'),
     path('getusers/', UserListView.as_view(), name='getusers'),
 ]
